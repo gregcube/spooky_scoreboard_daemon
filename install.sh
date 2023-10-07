4d7e48c574a3899f903946fbb27199bb32563b12
Greg <greg@pop-os.localdomain>
2023-10-06 23:55:50 -0400

    Clean up install script.

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Figure out if we're using wifi or ethernet.
-connect=""
 while getopts ":ew" opt; do
   case $opt in
     e) connect="ethernet";;
@@ -12,10 +11,18 @@ while getopts ":ew" opt; do
 done
 
 # Ensure -e or -w is used.
-if [ -z "$connect" ]; then
-  echo "Use -e for ethernet, or -w for wifi."
-  exit 1
-fi
+[ -n "$connect" ] || { echo "Use -e for ethernet, or -w for wifi." >&2; exit 1; }
+
+generate_ssb_script() {
+  cat <<EOF
+#!/bin/bash
+ip link set $1 up
+ip addr add $2 dev $1
+ip route add default via $3 dev $1
+/ssbd -md
+exit 0
+EOF
+}
 
 # Setup chroot environment.
 mount -o bind /dev /game3/dev
@@ -25,75 +32,47 @@ mount -t sysfs /sys /game3/sys
 
 # Find device name.
 dev=$(ip -4 -o addr show | awk '$2 != "lo" && $3 == "inet" {print $2}')
-if [ -z "$dev" ]; then
-  echo "Failed to determine device name."
-  exit 1
-fi
+[ -n "$dev" ] || { echo "Failed to determine device name." >&2; exit 1; }
 
 # Find current IP address and gateway.
 ip=$(ip -4 -o addr show $dev | awk '{print $4}')
 gw=$(ip -4 route show default | awk '/default/ {print $3}')
-if [ -z "$ip" ] || [ -z "$gw" ]; then
-    echo "Failed to determine IP address or gateway."
-    exit 1
-fi
+[ -z "$ip" ] || [ -z "$gw" ] && { echo "Failed to find IP address or gateway." >&2; exit 1; }
 
 # Create ssb.sh script for wifi or ethernet.
 if [ "$connect" = "wifi" ]; then
   mkdir /game3/etc/wpa_supplicant
   cp wifi.conf /game3/etc/wpa_supplicant
-  cat > /game3/ssb.sh <<EOF
-#!/bin/bash
-wpa_supplicant -B -D wext -i wlp0s20u3 -c /etc/wpa_supplicant/wifi.conf
-ip link set wlp0s20u3 up
-ip addr add $ip dev wlp0s20u3
-ip route add default via $gw dev wlp0s20u3
-/ssbd -md
-exit 0
-EOF
+  generate_ssb_script "wlp0s20u3" "$ip" "$gw" > /game3/ssb.sh
+  sed -i '2i\wpa_supplicant -B -Dwext -iwlp0s20u3 -c/etc/wpa_supplicant/wifi.conf' /game3/ssb.sh
 else
-  cat > /game3/ssb.sh <<EOF
-#!/bin/bash
-ip link set $dev up
-ip addr add $ip dev $dev
-ip route add default via $gw dev $dev
-/ssbd -md
-exit 0
-EOF
+  generate_ssb_script "$dev" "$ip" "$gw" > /game3/ssb.sh
 fi
 
 chmod +x /game3/ssb.sh
 
-# Add Google DNS to /game3/etc/resolv.conf
+# Use Google for DNS resolver.
 cat > /game3/etc/resolv.conf <<EOF
 nameserver 8.8.8.8
 nameserver 8.8.4.4
 EOF
 
 # Modify /game3/etc/X11/xinit/xinitrc
-# Execute ssb.sh script right before the game starts.
+# Execute ssb.sh right before the game starts.
 sed -i '/\.\/main\.x86_64$/i/ssb.sh' /game3/etc/X11/xinit/xinitrc
 
 # WiFi needs wpa_supplicant installed.
 # Use an older version because the host OS is dated.
 if [ "$connect" = "wifi" ]; then
   curl -L -o /game3/wpa.tar.xz https://archive.archlinux.org/packages/w/wpa_supplicant/wpa_supplicant-2%3A2.8-1-x86_64.pkg.tar.xz
-
-  if [ $? -ne 0 ]; then
-    echo "Failed to download wpa_supplicant."
-    exit 1
-  fi
-
+  [ $? -ne 0 ] && { echo "Failed to download wpa_supplicant." >&2; exit 1; }
   chroot /game3 pacman -U --noconfirm /wpa.tar.xz
 fi
 
 # Download and compile cJSON library.
 # Needed for ssbd to parse game audit file.
 curl -L -o /game3/cjson.tar.gz https://github.com/DaveGamble/cJSON/archive/refs/tags/v1.7.16.tar.gz
-if [ $? -ne 0 ]; then
-  echo "Failed to download cJSON."
-  exit 1
-fi
+[ $? -ne 0 ] && { echo "Failed to download cJSON." >&2; exit 1; }
 
 tar -xzf /game3/cjson.tar.gz -C /game3
 chroot /game3 /bin/bash -c "cd /cJSON-1.7.16 && make && exit"
