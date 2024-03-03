176cfea92c840e618df7c61a7e57e5eedf4427cf
Greg <greg@web.net>
2024-03-03 13:24:09 -0500

    use hwn for tcm starting point

--- src/GameBase.cpp
+++ src/GameBase.cpp
@@ -3,10 +3,12 @@
 #include "GameBase.h"
 #include "game/hwn.h"
 #include "game/tna.h"
+#include "game/tcm.h"
 
 std::map<std::string, GameFactoryFunction> gameFactories = {
   {"tna", []() { return std::make_unique<TNA>(); }},
-  {"hwn", []() { return std::make_unique<HWN>(); }}
+  {"hwn", []() { return std::make_unique<HWN>(); }},
+  {"tcm", []() { return std::make_unique<TCM>(); }}
 };
 
 std::unique_ptr<GameBase> GameBase::create(const std::string& gameName)
