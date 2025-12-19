baa27f3fd06c76cd87c99bcf19d10fdaf3d0631a
Greg <greg@web.net>
2025-12-19 15:02:23 -0500

    Version bump.

--- src/version.h
+++ src/version.h
@@ -2,9 +2,9 @@
 
 namespace Version {
   inline constexpr const char* MAJOR = "0";
-  inline constexpr const char* MINOR = "1";
-  inline constexpr const char* PATCH = "1";
+  inline constexpr const char* MINOR = "2";
+  inline constexpr const char* PATCH = "0";
   inline constexpr const char* BUILD = "1";
-  inline constexpr const char* FULL  = "0.1.1-1";
+  inline constexpr const char* FULL  = "0.2.0-1";
 }
 
