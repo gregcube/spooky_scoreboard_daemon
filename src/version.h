cb6863454e4d548327e28b83bb368722a3b79be9
Greg <greg@web.net>
2026-08-08 18:13:24 -0400

    Version bump to 0.3.0
    Remove old binaries; they're no longer compatible with server API changes.

--- src/version.h
+++ src/version.h
@@ -2,9 +2,9 @@
 
 namespace Version {
   inline constexpr const char* MAJOR = "0";
-  inline constexpr const char* MINOR = "2";
-  inline constexpr const char* PATCH = "3";
+  inline constexpr const char* MINOR = "3";
+  inline constexpr const char* PATCH = "0";
   inline constexpr const char* BUILD = "1";
-  inline constexpr const char* FULL  = "0.2.3-1";
+  inline constexpr const char* FULL  = "0.3.0-1";
 }
 
