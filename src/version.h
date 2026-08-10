2aa6902265e3c73ff39ff530d2d41221a4f378ff
Greg <greg@web.net>
2026-08-10 17:30:59 -0400

    Version bump to 0.3.1-1
    Changes:
    - Use signed JWT tokens for QR player login;
    - Fixes start-up bug when token has expired.

--- src/version.h
+++ src/version.h
@@ -3,8 +3,8 @@
 namespace Version {
   inline constexpr const char* MAJOR = "0";
   inline constexpr const char* MINOR = "3";
-  inline constexpr const char* PATCH = "0";
+  inline constexpr const char* PATCH = "1";
   inline constexpr const char* BUILD = "1";
-  inline constexpr const char* FULL  = "0.3.0-1";
+  inline constexpr const char* FULL  = "0.3.1-1";
 }
 
